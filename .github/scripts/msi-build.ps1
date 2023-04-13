7cf8b62fb22865b6badb0b6fb2bc2a2ff5e9c3c6
Edoardo Ceccarelli <ridleys@gmail.com>
2023-04-13 10:57:47 +0300

    removed transfer for debug

--- .github/scripts/msi-build.ps1
+++ .github/scripts/msi-build.ps1
@@ -74,7 +74,4 @@ if ($SignMsi)
     Install-Module MSI -Force
     Get-MSISummaryInfo rport-client.msi
     Get-AuthenticodeSignature rport-client.msi|Format-List
-
-    Write-Output "Transfer.sh $major.$minor.$patch"
-    curl --upload-file ./rport-client.msi https://transfer.sh/rport-clientV$major.$minor.$patch.msi
 }
